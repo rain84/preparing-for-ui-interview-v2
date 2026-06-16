3b5b87182ee39fe697957d0054555613b9c01a68
rain84 <rainy_sky@mail.ru>
2026-06-16 11:14:14 +0300

    Add solution for 01 Detect Type

--- src/problems/01-detect-type/detect-type.vanila.ts
+++ src/problems/01-detect-type/detect-type.vanila.ts
@@ -23,7 +23,8 @@ export type TType =
   | string
 
 export const detectType = (value: any): TType => {
-    throw new Error('Not implemented')
+  if (value == null) return `${value}`
+  return (Object.getPrototypeOf(value)?.constructor?.name ?? 'object').toLowerCase()
 }
 
 // --- Examples ---
